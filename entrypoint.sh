b3353e14650a5284a580efde7709ea91119197d3
Gustavo Stor <gustavostor@gmail.com>
2016-07-22 16:22:12 -0300

    Fixed cronjob env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -n "${INIT_BACKUP}" ]; then
+if [[ -n "${INIT_BACKUP}" ]]; then
     echo "=> Create a backup on the startup"
     ./mongo.sh
 fi
 
 echo "=> Adding backup crontab entry"
-echo "${CRON_TIME} /src/mongo.sh >> /mongo_sync.log 2>&1" >> /crontab.conf
+
+env > /crontab.conf
+echo "${CRON_TIME} /src/mongo.sh > /mongo_sync.log 2>&1" >> /crontab.conf
 
 touch /mongo_sync.log
 tail -f /mongo_sync.log &
